1fb3ff4a3baf3d333cb004290004154412e2d9d7
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-12-12 15:40:00 -0500

    created form to make new user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import NewUser from './users/createUser'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super()
+    this.state = {
+      img_url: ''
+    }
+  }
+  submitPhoto = (e) => {
+    e.preventDefault()
+    console.log(e)
+    let file = e.target[0].files[0]
+    let reader = new FileReader()
+    let url = reader.readAsDataURL(file)
+
+    reader.onloadend = function (e) {
+      this.setState({
+        img_url: [reader.result]
+      })
+    }.bind(this)
+
+    console.log('URL: ' ,url)
+
+  }
+  
+  render(){
+    return(
+      <>
+      {/* <h1>Hello World!</h1>
+      <form onSubmit={this.submitPhoto}>
+        <input type="file"></input>
+        <input type="submit" value="Submit"/>
+      </form>
+      <img src={this.state.img_url} alt="uploaded text"/> */}
+      <NewUser/>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ src/users/createUser.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react'
+import { Form, Button, Label, Header } from 'semantic-ui-react';
+import '../App.css'
+
+class NewUser extends Component{
+    constructor(props){
+        super()
+        this.state = {
+            username: '',
+            password: '',
+            name: '',
+            bio: '',
+            img: '',
+        }
+    }
+
+    createUser = (e) => {
+        e.preventDefault()
+        let formData = new FormData()
+
+        for(let i = 0; i < 5; i++){
+            if(e.target[i].name === "img"){
+                formData.append(e.target[i].name, e.target[i].files[0])
+            }
+            else{
+                formData.append(e.target[i].name, e.target[i].value)
+            }
+        }
+
+        // test that form data is being populated
+        for(var pair of formData.entries()){
+            console.log(pair[0]+ ", " + pair[1])
+        }
+        
+        // console.log('Create User event', e.target[0])
+        // console.log('Create User event', e)
+        // console.log('Create User event', e.target.length)
+        // console.log('Create User event', typeof(e.target))
+        
+    }
+
+    // method to change input fields
+    handleEditChange = (e) => {
+        this.setState({
+            [e.currentTarget.name]: [e.currentTarget.value]
+        })
+
+        console.log(this.state)
+    }
+
+    // previews the uploaded photo
+    addPhotoPreview = (e) => {
+        console.log(e)
+        let file = e.target.files[0]
+        let reader = new FileReader()
+        let url = reader.readAsDataURL(file)
+
+        reader.onloadend = function (e) {
+            this.setState({
+              img: [reader.result]
+            })
+          }.bind(this)
+
+        console.log('URL: ' ,url)
+    }
+
+    render(){
+        return(
+            <>
+            <Header>Register for Blogger!</Header>
+            <Form onSubmit={this.createUser}>
+                <Label>Full Name</Label>
+                <Form.Input type="text" 
+                            name="name"
+                            onChange={this.handleEditChange}
+                            value={this.state.name}/>
+
+                <Label>Username</Label>
+                <Form.Input type="text" 
+                            name="username"
+                            onChange={this.handleEditChange}
+                            value={this.state.username}/>
+
+                <Label>Password</Label>
+                <Form.Input type="password" 
+                            name="password"
+                            onChange={this.handleEditChange}
+                            value={this.state.password}/>
+
+                <Label>Bio</Label>
+                <Form.Input type="textarea" 
+                            name="bio"
+                            onChange={this.handleEditChange}
+                            value={this.state.bio}/>
+
+                <Label>Upload Image</Label>
+                <Form.Input type="file" name="img" onChange={this.addPhotoPreview}/>
+                <div>
+                    <img src={this.state.img} alt="uploaded" id="photo-preview"/>
+                </div>
+
+                <Form.Input type="submit" value="Create User"/>
+            </Form>
+            </>
+        )
+    }
+}
+
+export default NewUser;
