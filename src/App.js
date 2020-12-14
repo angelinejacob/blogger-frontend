bcc01a94978f8e37f582715981a2bac9de70db09
Angeline Jacob <angeline.jacob.17@gmail.com>
2020-12-14 16:54:22 -0500

    User Edit working and app updating as expected

--- src/App.js
+++ src/App.js
@@ -30,18 +30,20 @@ class App extends Component{
     console.log("Inside edit User method on app!")
     console.log(editedUser)
 
-    // Make API call to update the user
-    axios.put(`http://localhost:3000/users/${this.state.userId}`, editedUser)
-    .then((response) => {
-      console.log("response after update >>> ", response)
-      this.setState({
-        currentUser: response.data,
-        userId: response.data._id
-      })
-    })
-    .catch((error) => {
-      console.log("ERROR while updating user info: ", error)
-    })
+        axios({
+          method: 'put',
+          url: `http://localhost:3000/users/${this.state.userId}`,
+          data: editedUser,
+        })
+        .then((response) => {
+            console.log("successfully sent", response.data)
+            this.setState({
+              currentUser: response.data
+            })
+        })
+        .catch((error) => {
+            console.log("ERROR OCCURED: ", error)
+        })
   }
   
   render(){
